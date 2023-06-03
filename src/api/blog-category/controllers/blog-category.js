6eb57cb5a770dda2f077af7b7e243a831aeda533
devmunira <fswfofficial@gmail.com>
2023-06-03 20:52:31 +0600

    Blog and Product Model Done

---
+++ src/api/blog-category/controllers/blog-category.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * blog-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+
+
+module.exports = createCoreController('api::blog-category.blog-category');
